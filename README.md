bf3c0eec6ff92967c99e19b6f3429ffffb5b859c
Fernando Oliveira <47955596+lfpo2005@users.noreply.github.com>
2022-01-30 14:42:53 -0300

    final

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# -padrao-de-projeto-gof-Spring-
+# Desafio [DIO](https://dio.me/sign-up?ref=WDCKE2I7AM)
+
+# Padrões de projeto Gof com SpringBoot
+
+## Implementação
+
+Repositório com as implementações dos padrões de projeto. Desafio
+"Explorando Padrões de Projetos na Prática com Java", no projeto foi abordado
+3 padrões com o springBoot. 
+* Singleton
+* Strategy
+* Facade
+
+## Dependencias
+As seguintes dependenvias foram selecionados
+
+* Spring Data JPA
+* Spring Web
+* H2 DataBase
+* OpenFeign
+* Lombok
+
+### Autor
+
+Feito com ❤️ por Fernando Oliveira 👋🏽 Entre em contato!
+
+[![Linkedin Badge](https://img.shields.io/badge/-Fernando-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/lfpo2005/)](https://www.linkedin.com/in/lfpo2005/) 
+
--- src/main/java/dio/padrao/padraospring/service/ViaCepService.java
+++ src/main/java/dio/padrao/padraospring/service/ViaCepService.java
@@ -2,13 +2,13 @@ package dio.padrao.padraospring.service;
 
 import dio.padrao.padraospring.model.Endereco;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "viacep", url = "https://viacep.br/ws")
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
-    Endereco consultarCep(@PathVariable("cep")String cep);
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+
 }
